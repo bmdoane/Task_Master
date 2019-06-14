d5b8e472dace73b0fa985df224a85f218735affc
Brian Doane <brianmdoane@gmail.com>
2019-06-14 09:38:40 -0500

    Added autoFocus to input

--- src/components/GetTask.js
+++ src/components/GetTask.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function GetTask(props) {
   const isEnabled = props.userInput.length > 0
-  console.log('here', props);
   return (
     <form id="taskForm" onSubmit={props.handleSubmit}>
       <input
+        autoFocus
         type="text"
         placeholder="Task to master"
         onChange={props.handleChange}
